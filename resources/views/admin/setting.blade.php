a37d6fd50a41951aff99208ac188d212790d547e
gamzecemali <gamzecemali0@gmail.com>
2022-06-10 17:53:13 +0300

    "settings"

--- resources/views/admin/setting.blade.php
+++ resources/views/admin/setting.blade.php
@@ -14,7 +14,7 @@
         <div class="row">
             <!--  page header -->
             <div class="col-lg-12">
-                <h1 class="page-header">Tables</h1>
+                <h1 class="page-header">Settings</h1>
             </div>
             <!-- end  page header -->
         </div>
