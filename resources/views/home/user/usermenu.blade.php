67ee9d32285032557b0cd1efce664a9a7f6ad9eb
gamzecemali <gamzecemali0@gmail.com>
2022-06-12 20:27:42 +0300

    "User Panel Reviews &Comments"

--- resources/views/home/user/usermenu.blade.php
+++ resources/views/home/user/usermenu.blade.php
@@ -3,6 +3,5 @@
      <li><a href="#"><i class="fa fa-heart-o"></i>My Transfers</a></li>
      <li><a href="{{route('userpanel.Comment')}}"><i class="fa fa-exchange-o"></i>My Comments</a></li>
      <li><a href="#"><i class="fa fa-check-o"></i>Checkout</a></li>
-     <li><a href="#"><i class="fa fa-unlock-o"></i>My Products</a></li>
      <li><a href="#"><i class="fa fa-user-plus"></i>Logout</a></li>
  </ul>
